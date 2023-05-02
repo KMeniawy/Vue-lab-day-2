540e95f9a61d2920d12ac2059cc399fc63de1d23
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-05-02 15:56:22 +0200

    first commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+      <button
+        class="navbar-toggler poop"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse peep" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link" href="#"
+              >All Users</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/create" class="nav-link" href="#"
+              >Create User</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navBar",
+};
+</script>
+
+<style scoped>
+.navbar-layout {
+  display: flex;
+}
+.peep{
+  margin-left: 80%!important;
+}
+.poop{
+  margin-left: 90%!important;
+}
+</style>
